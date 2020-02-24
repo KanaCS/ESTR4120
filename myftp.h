a7bc67fa6eb688dfc467ae2e05bb0af67c27014a
OscarYau525 <40230274+OscarYau525@users.noreply.github.com>
2020-02-25 00:17:39 +0800

    add global variables #define

--- myftp.h
+++ myftp.h
@@ -7,3 +7,7 @@ struct message_s { //in total 10 bytes
 int sendn(int sd, void *buf, int buf_len);
 
 int recvn(int sd, void *buf, int buf_len);
+
+#define BATCH_SIZE 16384 // 2KB
+#define DPATH "data/"
+#define DPATH_LEN 5
