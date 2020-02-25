a850efb5e94671d5e694098f145d0b9763f8a452
KanaCS <49162615+KanaCS@users.noreply.github.com>
2020-02-25 15:34:50 +0800

    Update myftpserver.c

--- myftpserver.c
+++ myftpserver.c
@@ -121,33 +121,39 @@ void get(int sd, char *file_name) {
 }
  
 void put(int sd){
-   int len=0;
-   struct message_s PUT_REPLY;
-   struct message_s FILE_DATA;
-   char *buff = (char*)malloc(sizeof(char)*1024);
-   memset(buff, '\0', sizeof(char)*1024);
- 
-   strcpy(PUT_REPLY.protocol,"myftp");
-   PUT_REPLY.type = 0xC2;
-   PUT_REPLY.length = 10;
-   memcpy(buff, &PUT_REPLY, 10);
- 
-   if((len=sendn(sd,(void*)&PUT_REPLY,sizeof(PUT_REPLY)))<0){
-   	printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
-   	exit(0);
-   }
- 
-   if((len=recvn(sd,buff,sizeof(buff)))<0){
-   	printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
-   	exit(0);
-   }
-   memcpy(&FILE_DATA, buff, 10);
-   if(FILE_DATA.protocol[0]!='m' || FILE_DATA.protocol[1]!='y'|| FILE_DATA.protocol[2]!='f' && FILE_DATA.protocol[3]!='t' && FILE_DATA.protocol[4]!='p' || FILE_DATA.type != 0xFF || FILE_DATA.length != len){
-   	perror("file data put server failure\n");
-   	exit(1);   
-   }
- 
-   //unfinished
+	int len=0;
+	struct message_s PUT_REPLY;
+	struct message_s FILE_DATA;
+	char fullname[20];
+	strcpy(fullname, "data/");
+	strcat(fullname, file_name);
+	fullname[5+strlen(file_name)]='\0';
+	FILE *fp = fopen(fullname, "wb");
+	
+	char *buff = (char*)malloc(sizeof(char)*1024);
+	memset(buff, '\0', sizeof(char)*1024);
+
+	memcpy(PUT_REPLY.protocol,"myftp",5);
+	PUT_REPLY.type = 0xC2;
+	PUT_REPLY.length = 10;
+	memcpy(buff, &PUT_REPLY, 10);
+
+	if((len=sendn(sd,(void*)&PUT_REPLY,sizeof(PUT_REPLY)))<0){
+		printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
+		exit(0);
+	}
+
+	if((len=recvn(sd,buff,sizeof(buff)))<0){
+		printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
+		exit(0);
+	}
+	memcpy(&FILE_DATA,buff,10);
+
+	if(memcmp(&FILE_DATA, "myftp", 5) == 0 && FILE_DATA.type == 0xFF && len == FILE_DATA.length){
+		fwrite(&buff[10], 1, FILE_DATA.length-10, fp);
+	}
+	close(fp);
+
 }
  
  
