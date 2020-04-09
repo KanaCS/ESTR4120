d87e58ea4f1183c377391bf3d2f17d4767b0ba87
OscarYau525 <1155109029@link.cuhk.edu.hk>
2020-04-09 12:58:34 +0800

    bug

--- asg2/myftpclient.c
+++ asg2/myftpclient.c
@@ -145,9 +145,9 @@ void decode_file(int *effective_ids, char *filename, unsigned long long filesize
 			fread(file_data[i], 1, block_size, fp[i]);
 		}
 		if(err_count>0) {
-			printf("encode start\n");
+			// printf("encode start\n");
 			ec_encode_data(block_size, k, err_count, stripe->table, file_data, &file_data[k]);
-			printf("encode end\n");
+			// printf("encode end\n");
 		}
 		for(i = 0; i < k; i++) {
 			if(status[i] == 1) { // data row i is alive
@@ -168,11 +168,11 @@ void decode_file(int *effective_ids, char *filename, unsigned long long filesize
 		// 	printf(" %d", err_row_inds[i]);
 		// }
 		// printf("\n");
-		// printf("restore_order:");
-		// for(i = 0; i < k; i++) {
-		// 	printf(" %d", restore_order[i]);
-		// }
-		// printf("\n");
+		printf("restore_order:");
+		for(i = 0; i < k; i++) {
+			printf(" %d", restore_order[i]);
+		}
+		printf("\n");
 		// printf("remaining strip: %d\n", num_of_strip);
 		if(num_of_strip > 1) {
 			for(i = 0; i < k; i++) {
