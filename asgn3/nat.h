9bb37eab322e0ff7cf3febf2ccbb6da18591562e
KanaCS <49162615+KanaCS@users.noreply.github.com>
2020-05-20 16:50:26 +0800

    nat

--- asgn3/nat.h
+++ asgn3/nat.h
@@ -1,12 +1,24 @@
-run:
-sudo ./run.sh
-make
-sudo ./nat
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h> // required by "netfilter.h"
+#include <arpa/inet.h> // required by ntoh[s|l]()
+#include <signal.h> // required by SIGINT
+#include <string.h> // required by strerror()
+#include <sys/time.h> // required by gettimeofday()
+#include <time.h> // required by nanosleep()
+#include <errno.h> // required by errno
+# include <pthread.h>
+#include <netinet/ip.h>        // required by "struct iph"
+#include <netinet/tcp.h>    // required by "struct tcph"
+#include <netinet/udp.h>    // required by "struct udph"
+#include <netinet/ip_icmp.h>    // required by "struct icmphdr"
 
-when stop process and re-run
-run:
-sudo kill <process number>
-  (remark: process number can be check by "kill %")
-
-check the iptables rules:
-sudo iptables -L
+struct nat_tb{
+	struct in_addr itn_ip;
+	int itn_port;
+	struct in_addr trans_ip;
+	int trans_port;
+	struct timeval accesstv;
+	struct nat_tb* next;
+} typedef NAT_TB;
