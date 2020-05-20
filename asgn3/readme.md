edffa60ef33e08af6bc13203f20b3a484a4cce24
KanaCS <49162615+KanaCS@users.noreply.github.com>
2020-05-21 07:26:26 +0800

    v2 ver without multithread, testing method in remark on readme.md

--- asgn3/readme.md
+++ asgn3/readme.md
@@ -1,12 +1,17 @@
-run:
+**v2 is the one without multithreading and it work well**
+
+**run:**
 sudo ./run.sh
 make
 sudo ./nat
 
-when stop process and re-run
-run:
+**when stop process and re-run:**
+kill % //get the process number 
 sudo kill <process number>
-  (remark: process number can be check by "kill %")
 
-check the iptables rules:
+**check the iptables rules:**
 sudo iptables -L
+
+**testing:** 
+sudo ./nat 10.3.1.36 10.0.36.0 24 100 1 //vma
+echo "AAAA" | nc -uv 137.189.88.153 10100 //vmb or vmc
--- asgn3/v2/nat.c
+++ asgn3/v2/nat.c
@@ -51,22 +51,22 @@ void remove_expiry(){
       printf("true? %d\n",table->next==NULL);
       NAT_TB* befree = NULL;
       if(prev == NULL){
-  nat_table = table->next;
-  befree = table;
-  table = nat_table;
+	nat_table = table->next;
+	befree = table;
+	table = nat_table;
       }else{
         prev->next = table->next;
-  befree = table;
-  table = prev->next;
+	befree = table;
+	table = prev->next;
       }
       if(table!=NULL){
-  porttb[befree->trans_port-10000] = 'n';
+	porttb[befree->trans_port-10000] = 'n';
         free(befree);
       }
     }
     else{
-      prev = table;
-  table = table->next;
+    	prev = table;
+	table = table->next;
     }
   }
 printf("finish remove\n");
@@ -95,6 +95,7 @@ int add_token(unsigned int num) {
   pthread_mutex_unlock(&bucket_mutex);
   return 1;
 }
+
 int get_token() { // return when get 1 token successfully
   struct timespec tim1, tim2;
   double fill_time_second = 1.0 / bucket.rate;
@@ -164,10 +165,10 @@ int search_entry(struct in_addr ip, int port, struct timeval tv){
       nat_table->next = NULL;
       int i = 0;
       for(i=0;i<2000;i++){
-  if(porttb[i]=='n'){
-    porttb[i]='y';
-    break;
-  }
+	if(porttb[i]=='n'){
+	  porttb[i]='y';
+	  break;
+	}
       }
       nat_table->trans_port = i+10000;
       nat_table->accesstv = tv;
@@ -210,15 +211,20 @@ typedef struct trans_arg{
   nfqnl_msg_packet_hdr *header;
 } TransArg;
 
-void *translation_thread_run(void *arg) {
-  TransArg *param = (TransArg *)arg;
-  unsigned int id = param->id;
-  struct nfq_q_handle *myQueue = param->myQueue;
-  nfq_data* pkt = param->pkt;
-  nfqnl_msg_packet_hdr *header = param->header;
+static int Callback(struct nfq_q_handle *myQueue, struct nfgenmsg *msg,
+                nfq_data* pkt, void *cbData) {
+  unsigned int id = 0;
+  nfqnl_msg_packet_hdr *header;
+  printf("pkt recvd: ");
+  if ((header = nfq_get_msg_packet_hdr(pkt))) {
+          id = ntohl(header->packet_id);
+          printf("  id: %u\n", id);
+          printf("  hw_protocol: %u\n", ntohs(header->hw_protocol));       
+          printf("  hook: %u\n", header->hook);
+  }
 
   unsigned char *pktData;
- //int len = 
+  //int len = 
   nfq_get_payload(pkt, (unsigned char**)&pktData);
   struct iphdr *iph = (struct iphdr *)pktData;
   struct udphdr *udph =(struct udphdr *) (((char*)iph) + iph->ihl*4);
@@ -275,45 +281,6 @@ void *translation_thread_run(void *arg) {
   return nfq_set_verdict(myQueue, id, NF_ACCEPT, 0, NULL);
 }
 
-static int Callback(struct nfq_q_handle *myQueue, struct nfgenmsg *msg,
-                nfq_data* pkt, void *cbData) {
-  unsigned int id = 0;
-  nfqnl_msg_packet_hdr *header;
-
-  printf("pkt recvd: ");
-  if ((header = nfq_get_msg_packet_hdr(pkt))) {
-          id = ntohl(header->packet_id);
-          printf("  id: %u\n", id);
-          printf("  hw_protocol: %u\n", ntohs(header->hw_protocol));       
-          printf("  hook: %u\n", header->hook);
-  }
-
-  // check whether packet buffer have space left (buffer max = 10, to limit number of threads)
-  int success = 0;
-  pthread_mutex_lock(&packet_buffer_mutex);
-  if(packets_num_in_buffer < 10) {
-    packets_num_in_buffer += 1;
-    success = 1;
-  }
-  else {
-    printf("packet dropped due to limited buffer\n");
-  }
-  pthread_mutex_unlock(&packet_buffer_mutex);
-  if(success == 0) {
-    // drop this packet at router
-    return nfq_set_verdict(myQueue, id, NF_DROP, 0, NULL);
-  }
-
-  // packet will goto new thread and get translated then set_verdict accordingly
-  TransArg *arg = (TransArg *)malloc(sizeof(TransArg));
-  arg->header = header;
-  arg->id = id;
-  arg->myQueue = myQueue;
-  arg->pkt = pkt;
-  pthread_create(&translation_thread, NULL, translation_thread_run, arg);
-  return 0; // or required certain return?
-}
-
 int main(int argc, char** argv) {
 
   if(argc != 6){
@@ -323,18 +290,17 @@ int main(int argc, char** argv) {
   char ip_s[30],lan_s[30];
   strcpy(ip_s,argv[1]);
   strcpy(lan_s,argv[2]);
-  inet_aton(ip_s, &ip);
-  inet_aton(lan_s, &lan);
+  inet_aton(ip_s, &natip);
+  inet_aton(lan_s, &natlan);
   mask = atoi(argv[3]);
   int bsize = atoi(argv[4]);
   int rate = atoi(argv[5]);
-
+  
   //init porttb
   int i = 0;   
   for(i = 0; i<2000;i++){
     porttb[i]='n';
   }
-
   // Get a queue connection handle from the module
   struct nfq_handle *nfqHandle;
   if (!(nfqHandle = nfq_open())) {
