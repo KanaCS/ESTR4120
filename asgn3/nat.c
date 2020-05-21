34cb132e739909531a45ee5488384ddff404da5f
OscarYau525 <1155109029@link.cuhk.edu.hk>
2020-05-21 14:16:42 +0800

    added thread to recv pkt

--- asgn3/nat.c
+++ asgn3/nat.c
@@ -22,6 +22,7 @@ extern "C" {
 #include "checksum.h"
 
 #define BUF_SIZE 1500
+#define MAX_PKT_IN_BUF 10
 
 struct in_addr natip;
 struct in_addr natlan;
@@ -32,10 +33,9 @@ char porttb[2000];
 pthread_mutex_t bucket_mutex;
 TokenBucket bucket;
 
-pthread_mutex_t packet_buffer_mutex;
-unsigned int packets_num_in_buffer = 0;
-
-pthread_t translation_thread;
+pthread_mutex_t buf_mutex;
+int pkt_in_buf = 0;
+pthread_t recv_thread;
 
 
 void remove_expiry(){
@@ -51,22 +51,22 @@ void remove_expiry(){
       printf("true? %d\n",table->next==NULL);
       NAT_TB* befree = NULL;
       if(prev == NULL){
-  nat_table = table->next;
-  befree = table;
-  table = nat_table;
+	nat_table = table->next;
+	befree = table;
+	table = nat_table;
       }else{
         prev->next = table->next;
-  befree = table;
-  table = prev->next;
+	befree = table;
+	table = prev->next;
       }
       if(table!=NULL){
-  porttb[befree->trans_port-10000] = 'n';
+	porttb[befree->trans_port-10000] = 'n';
         free(befree);
       }
     }
     else{
-      prev = table;
-  table = table->next;
+    	prev = table;
+	table = table->next;
     }
   }
 printf("finish remove\n");
@@ -95,6 +95,7 @@ int add_token(unsigned int num) {
   pthread_mutex_unlock(&bucket_mutex);
   return 1;
 }
+
 int get_token() { // return when get 1 token successfully
   struct timespec tim1, tim2;
   double fill_time_second = 1.0 / bucket.rate;
@@ -164,10 +165,10 @@ int search_entry(struct in_addr ip, int port, struct timeval tv){
       nat_table->next = NULL;
       int i = 0;
       for(i=0;i<2000;i++){
-  if(porttb[i]=='n'){
-    porttb[i]='y';
-    break;
-  }
+	if(porttb[i]=='n'){
+	  porttb[i]='y';
+	  break;
+	}
       }
       nat_table->trans_port = i+10000;
       nat_table->accesstv = tv;
@@ -203,22 +204,50 @@ int search_entry(struct in_addr ip, int port, struct timeval tv){
   return 0;
 }
 
-typedef struct trans_arg{
-  unsigned int id;
-  struct nfq_q_handle *myQueue;
-  nfq_data* pkt;
-  nfqnl_msg_packet_hdr *header;
-} TransArg;
+typedef struct recv_thread_arg{
+  struct nfq_handle *nfqHandle;
+  char buf[BUF_SIZE];
+  int res;
+} RecvThreadArg;
 
-void *translation_thread_run(void *arg) {
-  TransArg *param = (TransArg *)arg;
-  unsigned int id = param->id;
-  struct nfq_q_handle *myQueue = param->myQueue;
-  nfq_data* pkt = param->pkt;
-  nfqnl_msg_packet_hdr *header = param->header;
+void *recv_thread_run(void *arg) {
+  RecvThreadArg *params = (RecvThreadArg *)arg;
+  nfq_handle_packet(params->nfqHandle, params->buf, params->res);
+  free(params);
+}
+
+int add_pkt_buf() {
+  int res = 0;
+  pthread_mutex_lock(&buf_mutex);
+  if(pkt_in_buf < MAX_PKT_IN_BUF) {
+    pkt_in_buf += 1;
+    res = 1;
+  }
+  pthread_mutex_unlock(&buf_mutex);
+  return res;
+}
+
+int rm_pkt_buf() {
+  pthread_mutex_lock(&buf_mutex);
+  pkt_in_buf -= 1;
+  pthread_mutex_unlock(&buf_mutex);
+  return 1;
+}
+
+static int Callback(struct nfq_q_handle *myQueue, struct nfgenmsg *msg,
+                nfq_data* pkt, void *cbData) {
+  unsigned int id = 0;
+  nfqnl_msg_packet_hdr *header;
+  printf("pkt recvd: ");
+  if ((header = nfq_get_msg_packet_hdr(pkt))) {
+          id = ntohl(header->packet_id);
+          printf("  id: %u\n", id);
+          printf("  hw_protocol: %u\n", ntohs(header->hw_protocol));       
+          printf("  hook: %u\n", header->hook);
+  }
 
   unsigned char *pktData;
- //int len = 
+  //int len = 
   nfq_get_payload(pkt, (unsigned char**)&pktData);
   struct iphdr *iph = (struct iphdr *)pktData;
   struct udphdr *udph =(struct udphdr *) (((char*)iph) + iph->ihl*4);
@@ -227,6 +256,11 @@ void *translation_thread_run(void *arg) {
   if (iph->protocol != IPPROTO_UDP) {
       printf("drop pkt other than udp\n");
       return nfq_set_verdict(myQueue, id, NF_DROP, 0, NULL);
+  }
+
+  if (add_pkt_buf() == 0) {
+    printf("drop pkt because pkt_in_buf > 10\n");
+    return nfq_set_verdict(myQueue, id, NF_DROP, 0, NULL);
   }
   unsigned int local_mask = 0xffffffff << (32 - mask);
   unsigned int lan_int = ntohl(natlan.s_addr);
@@ -260,6 +294,7 @@ void *translation_thread_run(void *arg) {
     int targetport;
     if(search_dst_port(dst_port, &targetip, &targetport, tv)==-1){
       printf("dst_port not in table, drop the pkt\n");
+      rm_pkt_buf();
       return nfq_set_verdict(myQueue, id, NF_DROP, 0, NULL);
     }
     memcpy(&(iph->daddr),&targetip,sizeof(targetip));
@@ -271,47 +306,9 @@ void *translation_thread_run(void *arg) {
   }
 
   printf("\n");
-
+  get_token(); // wait for token
+  rm_pkt_buf(); // reduce pkt_in_buf by 1
   return nfq_set_verdict(myQueue, id, NF_ACCEPT, 0, NULL);
-}
-
-static int Callback(struct nfq_q_handle *myQueue, struct nfgenmsg *msg,
-                nfq_data* pkt, void *cbData) {
-  unsigned int id = 0;
-  nfqnl_msg_packet_hdr *header;
-
-  printf("pkt recvd: ");
-  if ((header = nfq_get_msg_packet_hdr(pkt))) {
-          id = ntohl(header->packet_id);
-          printf("  id: %u\n", id);
-          printf("  hw_protocol: %u\n", ntohs(header->hw_protocol));       
-          printf("  hook: %u\n", header->hook);
-  }
-
-  // check whether packet buffer have space left (buffer max = 10, to limit number of threads)
-  int success = 0;
-  pthread_mutex_lock(&packet_buffer_mutex);
-  if(packets_num_in_buffer < 10) {
-    packets_num_in_buffer += 1;
-    success = 1;
-  }
-  else {
-    printf("packet dropped due to limited buffer\n");
-  }
-  pthread_mutex_unlock(&packet_buffer_mutex);
-  if(success == 0) {
-    // drop this packet at router
-    return nfq_set_verdict(myQueue, id, NF_DROP, 0, NULL);
-  }
-
-  // packet will goto new thread and get translated then set_verdict accordingly
-  TransArg *arg = (TransArg *)malloc(sizeof(TransArg));
-  arg->header = header;
-  arg->id = id;
-  arg->myQueue = myQueue;
-  arg->pkt = pkt;
-  pthread_create(&translation_thread, NULL, translation_thread_run, arg);
-  return 0; // or required certain return?
 }
 
 int main(int argc, char** argv) {
@@ -323,18 +320,17 @@ int main(int argc, char** argv) {
   char ip_s[30],lan_s[30];
   strcpy(ip_s,argv[1]);
   strcpy(lan_s,argv[2]);
-  inet_aton(ip_s, &ip);
-  inet_aton(lan_s, &lan);
+  inet_aton(ip_s, &natip);
+  inet_aton(lan_s, &natlan);
   mask = atoi(argv[3]);
   int bsize = atoi(argv[4]);
   int rate = atoi(argv[5]);
-
+  
   //init porttb
   int i = 0;   
   for(i = 0; i<2000;i++){
     porttb[i]='n';
   }
-
   // Get a queue connection handle from the module
   struct nfq_handle *nfqHandle;
   if (!(nfqHandle = nfq_open())) {
@@ -381,21 +377,19 @@ int main(int argc, char** argv) {
   bucket.tokens = bsize;
 
   // setup bucket mutex
-  if (pthread_mutex_init(&bucket_mutex, NULL) < 0) { 
-    printf("bucket_mutex init failed\n"); 
-    exit(-1);
-  } 
+  if (pthread_mutex_init(&bucket_mutex, NULL) < 0) { printf("bucket_mutex init failed\n"); exit(-1);} 
+  if (pthread_mutex_init(&buf_mutex, NULL) < 0) {  printf("buf_mutex init failed\n"); exit(-1);} 
 
-  if (pthread_mutex_init(&packet_buffer_mutex, NULL) < 0) { 
-    printf("packet_buffer_mutex init failed\n"); 
-    exit(-1);
-  } 
   // start filling bucket thread
   pthread_t bucket_thread;
   pthread_create(&bucket_thread, NULL, token_bucket_thread_run, NULL);
   
   while ((res = recv(fd, buf, sizeof(buf), 0)) && res >= 0) {
-    nfq_handle_packet(nfqHandle, buf, res);
+    RecvThreadArg *args = (RecvThreadArg *)malloc(sizeof(RecvThreadArg));
+    memcpy(args->buf, buf, res);
+    args->res = res;
+    args->nfqHandle = nfqHandle;
+    pthread_create(&recv_thread, NULL, recv_thread_run, args); // pass the recv pkt to a new thread, allow main to continue recv next pkt.
   }
 
   nfq_destroy_queue(nfQueue);
